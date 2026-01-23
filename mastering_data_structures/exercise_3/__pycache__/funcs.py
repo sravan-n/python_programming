66bedaa3f94d90be2ae17028b5c1fe15a02dd576
Sravan Kumar Nuthalapati <skn.sravan@gmail.com>
2026-01-23 17:39:02 -0600

    exercide3

---
+++ mastering_data_structures/exercise_3/__pycache__/funcs.py
@@ -0,0 +1,53 @@
+"""
+Functions demonstrating string methods.
+
+Neither this module nor any of these functions should import the introcs module.
+In addition, you are not allowed to use loops or recursion in either function.
+
+Author: Sravan Kumar Nuthalapati
+Date: 01/23/2026
+"""
+
+
+def first_in_parens(s):
+    """
+    Returns: The substring of s that is inside the first pair of parentheses.
+    
+    The first pair of parenthesis consist of the first instance of character
+    '(' and the first instance of ')' that follows it.
+    
+    Examples: 
+        first_in_parens('A (B) C') returns 'B'
+        first_in_parens('A (B) (C)') returns 'B'
+        first_in_parens('A ((B) (C))') returns '(B'
+    
+    Parameter s: a string to check
+    Precondition: s is a string with a matching pair of parens '()'.
+    """
+    
+    pos1 = s.find('(')
+    s = s[pos1+1:]
+    pos2 = s.find(')')
+
+    return s[:pos2]
+    
+
+
+def isnetid(s):
+    """
+    Returns True if s is a valid Cornell netid.
+    
+    Cornell network ids consist of 2 or 3 lower-case initials followed by a 
+    sequence of digits.
+    
+    Examples:
+        isnetid('wmw2') returns True
+        isnetid('2wmw') returns False
+        isnetid('ww2345') returns True
+        isnetid('w2345') returns False
+        isnetid('WW345') returns False
+    
+    Parameter s: the string to check
+    Precondition: s is a string
+    """
+    pass
