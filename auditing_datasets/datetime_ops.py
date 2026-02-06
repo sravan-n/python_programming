d186da9b16977e8af619866938169b8715ea4617
Sravan Kumar Nuthalapati <SravanKumar.Nuthalapati@charter.com>
2026-02-06 16:48:27 -0600

    feat(datetime_ops): Add time parsing and sunset lookup functions

--- auditing_datasets/datetime_ops.py
+++ auditing_datasets/datetime_ops.py
@@ -1,7 +1,8 @@
 """
-Functions for working with datetime objects / computing time elapsed.
+Functions for working with datetime objects / computing time elapsed / parsing time values.
 """
 import datetime
+from dateutil.parser import parse
 
 
 def christmas_day(year):
@@ -99,4 +100,72 @@ def past_a_week(d1,d2):
         return False
 
 
+# strftime('%Y-%m-%d' H:M:S)
+# isoformat()
+
+# dateutil.parse -> parse
+
+
+def str_to_time(timestamp):
+    """
+    Returns the datetime object for the given timestamp (or None if the stamp is invalid)
+    
+    This function should just use the parse function in dateutil.parser to convert the
+    timestamp to a datetime object.  If it is not a valid date (so the parser crashes), 
+    this function should return None.
+    
+    Parameter timestamp: The time stamp to convert
+    Precondition: timestamp is a string
+    """
+    # Hint: Use a try-except to return None if parsing fails
+    try:
+        return parse(timestamp)
+    except:
+        return None
+
+
+def sunset(date,daycycle):
+    """
+    Returns the sunset datetime (day and time) for the given date
+    
+    This function looks up the sunset from the given daycycle dictionary. If the
+    daycycle dictionary is missing the necessary information, this function 
+    returns the value None.
+    
+    A daycycle dictionary has keys for several years (as int).  The value for each year
+    is also a dictionary, taking strings of the form 'mm-dd'.  The value for that key 
+    is a THIRD dictionary, with two keys "sunrise" and "sunset".  The value for each of 
+    those two keys is a string in 24-hour time format.
+    
+    For example, here is what part of a daycycle dictionary might look like:
         
+        "2015": {
+            "01-01": {
+                "sunrise": "07:35",
+                "sunset":  "16:44"
+            },
+            "01-02": {
+                "sunrise": "07:36",
+                "sunset":  "16:45"
+            },
+            ...
+        }
+    
+    Parameter date: The date to check
+    Precondition: date is a date object
+    
+    Parameter daycycle: The daycycle dictionary
+    Precondition: daycycle is a valid daycycle dictionary, as described above
+    """
+    # HINT: ISO FORMAT IS 'yyyy-mm-ddThh:mm'.  Find the sunset value by constructing a
+    # string in ISO format and calling str_to_time.
+    
+    year_str = str(date.year)
+    date_str = date.strftime('%m-%d')
+
+    try:
+        sunset = daycycle[year_str][date_str]['sunset']
+        sunset_iso = date.strftime('%Y-%m-%d')+'T'+sunset
+        return str_to_time(sunset_iso)
+    except:
+        return None
