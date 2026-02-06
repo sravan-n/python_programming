d406ff6dfc60b9427f13d0350cc0cf6f133f39b0
Sravan Kumar Nuthalapati <SravanKumar.Nuthalapati@charter.com>
2026-02-06 11:59:07 -0600

    feat(datetime_ops): Add past_a_week function for week-based date comparison

--- auditing_datasets/datetime_ops.py
+++ auditing_datasets/datetime_ops.py
@@ -1,5 +1,5 @@
 """
-Functions for working with datetime objects.
+Functions for working with datetime objects / computing time elapsed.
 """
 import datetime
 
@@ -65,3 +65,38 @@ def is_before(d1,d2):
             d2 = datetime.datetime(d2.year, d2.month, d2.day)
         
         return d1 < d2
+
+
+def past_a_week(d1,d2):
+    """
+    Returns True if event d2 happens at least a week (7 days) after d1.
+    
+    If d1 is after d2, or d2 is less than a week after d1, this function returns False.
+    Values d1 and d2 can EITHER be date objects or datetime objects.  If a date object,
+    assume that it happens at midnight of that day. 
+    
+    Parameter d1: The first event
+    Precondition: d1 is EITHER a date object or a datetime object
+    
+    Parameter d2: The second event
+    Precondition: d2 is EITHER a date object or a datetime object
+    """
+    
+    type_d1 = type(d1)
+    type_d2 = type(d2)
+
+    if type_d1 == datetime.date:
+        d1 = datetime.datetime(d1.year, d1.month, d1.day)
+
+    if type_d2 == datetime.date:
+        d2 = datetime.datetime(d2.year, d2.month, d2.day)
+
+    td = d2 - d1
+
+    if td.days >= 7:
+        return True
+    else:
+        return False
+
+
+        
